4306d79eaf483acca6dda40fa06f39a77ef8bae0
Xubuntu <marcel.amsler@gmail.com>
2014-10-15 11:18:55 +0200

    added main.cpp

--- ws5/words/word.cpp
+++ ws5/words/word.cpp
@@ -8,14 +8,24 @@
 Word::Word(std::string wordinput):word{wordinput}
 {
 	for_each(wordinput.begin(), wordinput.end(), [](const char c)
-			{if (!std::isalpha(c))
+		{
+			if (!std::isalpha(c))
 				throw std::out_of_range("invalid character");
-			});
+		});
 }
 
-Word::readWord(std::istream &in){
+void Word::readWord(std::istream &in){
 
+using input = std::istream_iterator<char>;
+input eof {};
 
+for_each(input{in}, eof,[&] (const char c){
+	if(std::isalpha(c)){
+		word.push_back(c);
+	} else {
+		return;
+	}
+});
 
 }
 
--- ws5/words/word.h
+++ ws5/words/word.h
@@ -2,17 +2,22 @@
 #define WORD_H_
 
 #include <string>
-#include <iosfwd>
+#include <iostream>
 
 struct Word {
 	std::string word;
-	Word(std::string wordInput):word{wordInput}{};
+	Word(std::string wordInput);
 	void readWord(std::istream &in);
 };
 
-inline std::istream operator >>(std::istream &in, Word &word){
+inline std::istream& operator >>(std::istream &in, Word &word){
 	word.readWord(in);
 	return in;
+}
+
+inline std::ostream& operator << (std::ostream &out, Word &word){
+	out << word.word;
+	return out;
 }
 
 
