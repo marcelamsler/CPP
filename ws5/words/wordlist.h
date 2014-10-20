f3f06cc68a307bc9891bffb0c2963a688cdbe858
Xubuntu <marcel.amsler@gmail.com>
2014-10-20 17:20:08 +0200

    added word-list to words-lib

---
+++ ws5/words/wordlist.h
@@ -0,0 +1,12 @@
+#ifndef WORDLIST_H_
+#define WORDLIST_H_
+
+#include "word.h"
+#include <iosfwd>
+#include <vector>
+
+std::vector<Word> createWordList(std::istream &in);
+
+
+
+#endif /* WORDLIST_H_ */
