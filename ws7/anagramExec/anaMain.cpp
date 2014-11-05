0825195af0edc42d4c5b3641adfd819c1321ac3b
Xubuntu <marcel.amsler@gmail.com>
2014-11-05 09:04:27 +0100

    fixed Anagrams to find only valid ones

--- ws7/anagramExec/anaMain.cpp
+++ ws7/anagramExec/anaMain.cpp
@@ -13,14 +13,14 @@ int main (){
 	myfile.open("/usr/share/dict/words");
 
 	std::istream_iterator<std::string> it{myfile};
-	std::istream_iterator<std::string> it2{myfile};
 
 	std::istream_iterator<std::string> eof{};
 
 	while (it != eof) {
 		std::set<std::string> anagram_set{getAllAnagrams(*it)};
 		std::vector<std::string> valid_anagrams{};
-		std::set_intersection(anagram_set.begin(), anagram_set.end(), it2, eof, valid_anagrams.begin());
+		std::set_intersection(anagram_set.begin(), anagram_set.end(), it, eof,
+				std::inserter(valid_anagrams, valid_anagrams.begin()));
 		set.insert(valid_anagrams.begin(), valid_anagrams.end());
 		it++;
 	}
