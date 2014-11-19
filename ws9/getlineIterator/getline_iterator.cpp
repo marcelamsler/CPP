0dadd1080458e3f8f2b101b1963d3cac3084aeb3
Xubuntu <marcel.amsler@gmail.com>
2014-11-19 08:47:21 +0100

    added iterator Lib and Test

---
+++ ws9/getlineIterator/getline_iterator.cpp
@@ -0,0 +1,10 @@
+/*
+ * getline_iterator.cpp
+ *
+ *  Created on: Nov 19, 2014
+ *      Author: hsr
+ */
+
+
+
+
---
+++ ws9/getlineIterator/getline_iterator.h
@@ -0,0 +1,15 @@
+/*
+ * getline_iterator.h
+ *
+ *  Created on: Nov 19, 2014
+ *      Author: hsr
+ */
+
+#ifndef GETLINE_ITERATOR_H_
+#define GETLINE_ITERATOR_H_
+
+
+
+
+
+#endif /* GETLINE_ITERATOR_H_ */
