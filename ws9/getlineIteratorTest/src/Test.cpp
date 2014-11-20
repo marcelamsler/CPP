d23319b5bdf2d0e3ac770f7e0930ab4bd96f31fc
Xubuntu <marcel.amsler@gmail.com>
2014-11-20 10:42:44 +0100

    added test for emptylastline

--- ws9/getlineIteratorTest/src/Test.cpp
+++ ws9/getlineIteratorTest/src/Test.cpp
@@ -36,12 +36,19 @@ void testEmptyFirstLine() {
 	ASSERT_NOT_EQUAL_TO(getlineIterator{emptyLine}, getlineIterator{});
 }
 
+void testEmptyLastLine() {
+	std::istringstream emptyLastLine {"bla\n bla \n\n"};
+	std::vector<std::string> v {getlineIterator{emptyLastLine}, getlineIterator{}};
+	ASSERT_EQUAL(3, v.size());
+}
+
 void runAllTests(int argc, char const *argv[]){
 	cute::suite s;
 	s.push_back(CUTE(testMultipleLines));
 	s.push_back(CUTE(testEqualityEOF));
 	s.push_back(CUTE(testDereferencingAndOperators));
 	s.push_back(CUTE(testEmptyFirstLine));
+	s.push_back(CUTE(testEmptyLastLine));
 
 	cute::xml_file_opener xmlfile(argc,argv);
 	cute::xml_listener<cute::ide_listener<> >  lis(xmlfile.out);
