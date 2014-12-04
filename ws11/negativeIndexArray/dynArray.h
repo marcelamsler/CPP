4bfd372a2d460759c3c6a1c2795e4daf7bd326b7
Xubuntu <marcel.amsler@gmail.com>
2014-12-04 15:03:30 +0100

    added iterators

--- ws11/negativeIndexArray/dynArray.h
+++ ws11/negativeIndexArray/dynArray.h
@@ -2,11 +2,16 @@
 #define DYNARRAY_H_
 
 #include <vector>
+#include <iterator>
 
 template <typename T>
 class dynArray {
 	using VectorType = std::vector<T>;
 	using size_type = typename VectorType::size_type;
+	using iterator = typename VectorType::iterator;
+	using const_iterator = typename VectorType::const_iterator;
+	using reverse_iterator = typename VectorType::reverse_iterator;
+	using reverse_const_iterator = typename VectorType::const_reverse_iterator;
 	VectorType vec{};
 
 public:
@@ -31,6 +36,46 @@ public:
 	T& operator[](int index) {
 		return at(index);
 	}
+
+	//iterator-functions
+
+	iterator begin () {
+		return vec.begin();
+	}
+
+	const_iterator cbegin(){
+		return vec.cbegin();
+	}
+
+	iterator end () {
+			return vec.end();
+	}
+
+	const_iterator cend(){
+			return vec.cend();
+	}
+
+	iterator rbegin () {
+		return vec.rbegin();
+	}
+
+	const_iterator crbegin(){
+		return vec.crbegin();
+	}
+
+	iterator rend () {
+			return vec.rend();
+	}
+
+	const_iterator crend(){
+			return vec.crend();
+	}
+
+
+
+
+
+
 
 };
 
